bb9adfa72dbcd03a3dcc0d7148667599262776c9
nan <tagjmp@gmail.com>
2018-07-08 23:28:30 +0900

    make compatible with voicetext

--- google-home-notifier.js
+++ google-home-notifier.js
@@ -1,111 +1,95 @@
-var Client = require('castv2-client').Client;
-var DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
-var mdns = require('mdns');
-var browser = mdns.createBrowser(mdns.tcp('googlecast'));
-var deviceAddress;
-var language;
+/// <reference path="node_modules/@types/node/index.d.ts" />
+'use strict'
 
-var device = function(name, lang = 'en') {
-    device = name;
-    language = lang;
+let VoiceText = require('voicetext');
+let Client = require('castv2-client').Client;
+let DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
+let mdns = require('mdns');
+
+class GoogleHomeNotifier {
+  constructor(voiceTextKey, audioFile, audioUrl) {
+    this.voicetext = new VoiceText(voiceTextKey);
+    this.audioFile = audioFile;
+    this.audioUrl = audioUrl;
+    this.setting = {
+      speaker: this.voicetext.SPEAKER.HIKARI,
+      pitch: 100,
+      speed: 100,
+      volume: 100
+    };
+  }
+
+  setDeviceName(name) {
+    this.deviceName = name;
     return this;
-};
-
-var ip = function(ip, lang = 'en') {
-  deviceAddress = ip;
-  language = lang;
-  return this;
-}
-
-var googletts = require('google-tts-api');
-var googlettsaccent = 'us';
-var accent = function(accent) {
-  googlettsaccent = accent;
-  return this;
-}
-
-var notify = function(message, callback) {
-  if (!deviceAddress){
-    browser.start();
-    browser.on('serviceUp', function(service) {
-      console.log('Device "%s" at %s:%d', service.name, service.addresses[0], service.port);
-      if (service.name.includes(device.replace(' ', '-'))){
-        deviceAddress = service.addresses[0];
-        getSpeechUrl(message, deviceAddress, function(res) {
-          callback(res);
-        });
-      }
-      browser.stop();
-    });
-  }else {
-    getSpeechUrl(message, deviceAddress, function(res) {
-      callback(res);
-    });
   }
-};
 
-var play = function(mp3_url, callback) {
-  if (!deviceAddress){
-    browser.start();
-    browser.on('serviceUp', function(service) {
-      console.log('Device "%s" at %s:%d', service.name, service.addresses[0], service.port);
-      if (service.name.includes(device.replace(' ', '-'))){
-        deviceAddress = service.addresses[0];
-        getPlayUrl(mp3_url, deviceAddress, function(res) {
-          callback(res);
-        });
-      }
-      browser.stop();
-    });
-  }else {
-    getPlayUrl(mp3_url, deviceAddress, function(res) {
-      callback(res);
-    });
+  setDeviceAddress(address) {
+    this.deviceAddress = address;
+    return this;
   }
-};
 
-var getSpeechUrl = function(text, host, callback) {
-  googletts(text, language, 1, 1000, googlettsaccent).then(function (url) {
-    onDeviceUp(host, url, function(res){
-      callback(res)
-    });
-  }).catch(function (err) {
-    console.error(err.stack);
-  });
-};
+  notify(message, callback) {
+    if (!this.deviceAddress) {
+      let browser = mdns.createBrowser(mdns.tcp('googlecast'));
+      browser.start();
+      browser.on('serviceUp', function (service) {
+        console.log('Device "%s" at %s:%d', service.name, service.addresses[0], service.port);
+        if (service.name.includes(this.device.replace(' ', '-'))) {
+          this.deviceAddress = service.addresses[0];
+          this.getSpeechUrl(message, this.deviceAddress, function (res) {
+            callback(res);
+          });
+        }
+        browser.stop();
+      });
+    } else {
+      this.getSpeechUrl(message, this.deviceAddress, function (res) {
+        callback(res);
+      });
+    }
+  };
 
-var getPlayUrl = function(url, host, callback) {
-    onDeviceUp(host, url, function(res){
-      callback(res)
-    });
-};
+  getSpeechUrl(text, host, callback) {
+    this.voicetext
+      .speaker(this.setting.speaker)
+      .pitch(this.setting.pitch)
+      .speed(this.setting.speed)
+      .volume(this.setting.volume)
+      .speak(text, (e, buf) => {
+        const fs = require('fs');
 
-var onDeviceUp = function(host, url, callback) {
-  var client = new Client();
-  client.connect(host, function() {
-    client.launch(DefaultMediaReceiver, function(err, player) {
+        fs.writeFile(this.audioFile, buf, 'binary', (e) => {
+          this.onDeviceUp(host, this.audioUrl, callback);
+        });
+      });
+  };
 
-      var media = {
-        contentId: url,
-        contentType: 'audio/mp3',
-        streamType: 'BUFFERED' // or LIVE
-      };
-      player.load(media, { autoplay: true }, function(err, status) {
-        client.close();
-        callback('Device notified');
+  onDeviceUp(host, url, callback) {
+    var client = new Client();
+    client.connect(host, function () {
+      client.launch(DefaultMediaReceiver, function (err, player) {
+        var media = {
+          contentId: url,
+          contentType: 'audio/wav',
+          streamType: 'BUFFERED'
+        };
+        console.log(url);
+        player.load(media, { autoplay: true }, function (err, status) {
+          client.close();
+          console.log(err, status);
+          callback('Device notified');
+        });
       });
     });
-  });
 
-  client.on('error', function(err) {
-    console.log('Error: %s', err.message);
-    client.close();
-    callback('error');
-  });
-};
+    client.on('error', function (err) {
+      console.log('Error: %s', err.message);
+      client.close();
+      callback('error');
+    });
+  };
 
-exports.ip = ip;
-exports.device = device;
-exports.accent = accent;
-exports.notify = notify;
-exports.play = play;
+}
+
+module.exports = GoogleHomeNotifier;
